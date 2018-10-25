56cfd21ce14acb250732639eca6cab4c002c8d0a
andrade97 <36697596+andrade97@users.noreply.github.com>
2018-10-25 08:44:33 -0300

    Add files via upload

---
+++ src/main/java/br/inatel/EC206/integracaocontinua/IntegracaoContinua.java
@@ -0,0 +1,9 @@
+package br.inatel.EC206.integracaocontinua;
+
+public class IntegracaoContinua {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
